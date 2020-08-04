961b667dca9e567a70f70950cfb45fec38d460ec
Charles Harvey <charlie.harvey@beable.com>
2020-08-04 16:10:08 -0400

    Force the prototype of the exceptions since it is lost when extending Typescript's Error

--- lib/exceptions/tracing-not-initialized.exception.ts
+++ lib/exceptions/tracing-not-initialized.exception.ts
@@ -3,5 +3,7 @@ export class TracingNotInitializedException extends Error {
     super(
       `Tracing was used in a code path that is not wrapped in AsyncContext#run. Please make sure that the appropriate middleware/interceptor for your environment is executed.`
     );
+
+    Object.setPrototypeOf(this, TracingNotInitializedException.prototype);
   }
 }
