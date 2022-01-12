39ab6d72ff8078c3a935ed1478230f71f8edca84
Marc Mogdanz <hey@marcmogdanz.de>
2022-01-12 22:58:03 +0100

    fix(test): broken imports of SegmentEmitter

--- test/util/segmentEmitterMock.ts
+++ test/util/segmentEmitterMock.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import * as SegmentEmitter from "aws-xray-sdk-core/lib/segment_emitter.js";
+import * as SegmentEmitter from "aws-xray-sdk-core/dist/lib/segment_emitter.js";
 
 export const segmentEmitterMock = () =>
   jest.spyOn(SegmentEmitter, "send").mockImplementation(() => {});
